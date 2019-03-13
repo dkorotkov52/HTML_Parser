d7acc4616e85d39c528f4aa77f8c3f43df8d8a77
korotkov52 <dkorotkov52@yandex.ru>
2019-03-13 22:39:28 +0300

    logic

--- app/src/main/java/com/example/html_parser/MainActivity.java
+++ app/src/main/java/com/example/html_parser/MainActivity.java
@@ -1,16 +1,34 @@
 package com.example.html_parser;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
+    TextView parse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        loadHTML();
     }
 
+    private void initView() {
+        getSupportActionBar().setTitle("Parsing HTML");
+        myWebView = findViewById(R.id.web_view);
+        parse = findViewById(R.id.parse);
+    }
 
-
+    private void loadHTML() {
+        myWebView.loadUrl("file:///android_asset/list.html");
+    }
 }
